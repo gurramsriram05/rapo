6236b99af2123718e0c9fdfe603933ff5d6cce7e
unknown <sriramchowdhary4@gmail.com>
2022-07-02 14:20:08 +0530

    Adding numbers program

---
+++ AddTwoNumbers.py
@@ -0,0 +1,10 @@
+# Python3 program to add two numbers
+
+num1 = 20
+num2 = 30
+
+# Adding two nos
+sum = num1 + num2
+
+# printing values
+print("Sri Ram create Sum of two num Sum:  of {0} and {1} is {2}" .format(num1, num2, sum))
